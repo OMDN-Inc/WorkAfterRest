38f2bae74136b5a839547cf74d65a188b4cdc96d
Simosv-Flan <62529783+mr2024088332@users.noreply.github.com>
2023-07-14 13:45:32 +0800

    Add files via upload

---
+++ WorkAfterRest/Main11.xaml.vb
@@ -0,0 +1,28 @@
+﻿Imports System.Windows.Threading
+Public Class Main11
+    Private WithEvents timer As DispatcherTimer
+    Private Num As Int32
+    Private Sub Timer_Tick(ByVal sender As Object, ByVal e As EventArgs) Handles Timer.Tick
+        ' 在每个Tick事件中执行需要定期执行的代码
+        Num = Num + 1
+        LabelU.Content = Num & "%"
+        If Num > 100 Then
+            Num = 0
+        End If
+    End Sub
+
+    Private Sub Main_KeyDown(sender As Object, e As KeyEventArgs) Handles Me.KeyDown
+        If e.Key = Key.Escape Then
+            Close()
+        End If
+    End Sub
+    Private Sub Main11_Loaded(sender As Object, e As RoutedEventArgs) Handles Me.Loaded
+        timer = New DispatcherTimer()
+        timer.Interval = TimeSpan.FromSeconds(3) ' 设置定时器间隔，这里以1秒为例
+        timer.Start() ' 启动定时器
+    End Sub
+
+    Private Sub MainWindow_Closed(ByVal sender As Object, ByVal e As EventArgs) Handles Me.Closed
+        timer.Stop() ' 停止定时器
+    End Sub
+End Class
---
+++ WorkAfterRest/MainWindow.xaml.vb
@@ -0,0 +1,21 @@
+﻿Class MainWindow 
+
+
+    Private Sub Exbutton1_Click(sender As Object, e As RoutedEventArgs) Handles Exbutton1.Click
+        Dim main As New Main
+        main.Show()
+    End Sub
+
+    Private Sub Labelx_MouseLeftButtonDown(sender As Object, e As MouseButtonEventArgs) Handles Labelx.MouseLeftButtonDown
+        Try
+            Process.Start("https://github.com/OMDN-Inc/WorkAfterRest")
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+    Private Sub Exbutton2_Click(sender As Object, e As RoutedEventArgs) Handles Exbutton2.Click
+        Dim main11 As New Main11
+        main11.Show()
+    End Sub
+End Class
